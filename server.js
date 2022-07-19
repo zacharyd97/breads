36448c9d349e08459d7a1e9d192bb34b6cd6c9f1
Zachary Dowell <zdowell97@gmail.com>
2022-07-19 18:36:36 -0400

    added routes

--- server.js
+++ server.js
@@ -15,3 +15,12 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
