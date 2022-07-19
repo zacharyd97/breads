deb8a1825b3a3d478e8dce673f02cb327541b7f7
Zachary Dowell <zdowell97@gmail.com>
2022-07-19 18:38:35 -0400

    fixed scope issue

--- server.js
+++ server.js
@@ -11,16 +11,14 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+
   
