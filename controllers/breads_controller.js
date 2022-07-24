91ae09b482663c050518667a1143b9d541d16b4d
Zachary Dowell <zdowell97@gmail.com>
2022-07-24 13:45:22 -0400

    edit and update routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,6 +16,15 @@ breads.get("/new", (req, res) => {
   res.render("new");
 });
 
+// EDIT
+breads.get('/:arrayIndex/edit', (req, res) => {
+  res.render('edit', {
+    bread: bread[req.params.arrayIndex],
+    index: req.params.arrayIndex
+  })
+})
+
+
 // SHOW
 
 breads.get("/:arrayIndex", (req, res) => {
@@ -37,14 +46,25 @@ breads.post("/", (req, res) => {
   } else {
     req.body.hasGluten = "false";
   }
-  Bread.push(req.body);
+  bread.push(req.body);
   res.redirect("/breads");
 });
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
+breads.delete('/:arrayIndex', (req, res) => {
+  bread.splice(req.params.arrayIndex, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function show ({bread}, index) {
+function show ({bread, index}) {
     console.log("bread inside show",bread)
     return (
         <Default>
@@ -16,11 +16,11 @@ function show ({bread}, index) {
     have gluten.
   </p>
   <img src={bread.image} alt={bread.name} />
-  <li><a href="/breads">Go home</a></li>
-  
-    <form action={`/breads/${index}?_method=DELETE`} method="POST">
-      <input type='submit' value="DELETE"/>
-    </form>
+  <a href="/breads"><button>Go home</button></a>
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+<form action={`/breads/${index}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+</form>
 
 </Default>
     
