1c422ab75f7856d2c0cc1a495607b3b77ae0a85c
Zachary Dowell <zdowell97@gmail.com>
2022-08-09 20:16:45 -0400

    fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an awesome App about Breads')
+  res.render('index')
 })
 
 // Breads
